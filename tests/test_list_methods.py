8cd5a4eee91666dba7fea694b7a7434062953402
Angelica <Angelica137@users.noreply.github.com>
2022-02-24 21:30:46 +0000

    test find_my_frined(), red

--- tests/test_list_methods.py
+++ tests/test_list_methods.py
@@ -9,3 +9,8 @@ def test_add_to_queue_0():
 def test_add_to_queue_1():
     assert add_to_queue(express_q=["Tony", "Bruce"], normal_q=["RobotGuy", "WW"],
                         ticket=1, name="RichieRich") == ["Tony", "Bruce", "RichieRich"]
+
+
+def test_find_my_friend():
+    assert find_my_friend(
+        queue=["Natasha", "Steve", "T'challa", "Wanda", "Rocket"], friend_name="Steve") == 1
