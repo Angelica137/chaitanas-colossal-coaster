90aceff9b10c800effbbcd952ef1d1cb485cb069
Angelica <Angelica137@users.noreply.github.com>
2022-02-24 21:22:35 +0000

    update add_to_queue() test case

--- tests/test_list_methods.py
+++ tests/test_list_methods.py
@@ -1,6 +1,6 @@
 from scripts.list_methods import *
 
 
-def test_add_to_queue():
+def test_add_to_queue_0():
     assert add_to_queue(express_q=["Tony", "Bruce"], normal_q=[
         "RobotGuy", "WW"], ticket=0, name="HawkEye") == ["RobotGuy", "WW", "HawkEye"]
