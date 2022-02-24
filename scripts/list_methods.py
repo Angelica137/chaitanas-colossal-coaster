d8419f320a4351fe85b6fe96cac7c033a02d10b7
Angelica <Angelica137@users.noreply.github.com>
2022-02-24 21:21:23 +0000

    remove print statements

--- scripts/list_methods.py
+++ scripts/list_methods.py
@@ -9,17 +9,3 @@ def add_to_queue(express_q: list, normal_q: list, ticket: int, name: str) -> lis
         return express_q
     normal_q.append(name)
     return normal_q
-
-
-print(add_to_queue(express_q=["Tony", "Bruce"], normal_q=[
-    "RobotGuy", "WW"], ticket=0, name="HawkEye"))
-
-
-express_q = ["Tony", "Bruce"]
-normal_q = ["RobotGuy", "WW"]
-ticket = 0
-name = "HawkEye"
-
-print(normal_q.append(name))
-normal_q.append(name)
-print(normal_q)
