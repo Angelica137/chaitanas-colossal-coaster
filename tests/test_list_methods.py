7d5a6746013496401b6a4fd2f46678eed9fec45e
Angelica <Angelica137@users.noreply.github.com>
2022-02-24 21:28:10 +0000

    test add_to_queue() express queue, green

--- tests/test_list_methods.py
+++ tests/test_list_methods.py
@@ -7,5 +7,5 @@ def test_add_to_queue_0():
 
 
 def test_add_to_queue_1():
-    assert add_to_queue(["Tony", "Bruce"], normal_queue=["RobotGuy", "WW"],
-                        ticket_type=1, person_name="RichieRich") == ["Tony", "Bruce", "RichieRich"]
+    assert add_to_queue(express_q=["Tony", "Bruce"], normal_q=["RobotGuy", "WW"],
+                        ticket=1, name="RichieRich") == ["Tony", "Bruce", "RichieRich"]
