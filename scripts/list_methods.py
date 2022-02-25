112fd260b84e6b9134e880be3e1e2bc398902dd0
Angelica <Angelica137@users.noreply.github.com>
2022-02-25 21:12:53 +0000

    refactor add_to_queue

--- scripts/list_methods.py
+++ scripts/list_methods.py
@@ -4,11 +4,7 @@ def add_to_queue(express_q: list, normal_q: list, ticket: int, name: str) -> lis
     :param ticket: stores the ticket type where 1 = express queue and
     0 = normal queue 
     """
-    if ticket == 1:
-        express_q.append(name)
-        return express_q
-    normal_q.append(name)
-    return normal_q
+    return (express_q if ticket == 1 else normal_q) + [name]
 
 
 def find_my_friend(queue: list, friend_name: str) -> int:
